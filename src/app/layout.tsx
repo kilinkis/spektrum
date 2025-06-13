52d4544b91cb1977be66674d5dba4b2cfe036314
Juan Manuel Incaurgarat <juanofx@gmail.com>
2025-06-13 08:05:03 +0200

    Added Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,10 @@
+import { Analytics } from "@vercel/analytics/next"
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>{children}</body>
+      <Analytics />
     </html>
   );
 }
